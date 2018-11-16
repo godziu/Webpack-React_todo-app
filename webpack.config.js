02094680fc8737a87a786f7360d85f87918fa9ca
Damian Godek <damian.godek1@gmail.com>
2018-11-16 19:25:32 +0100

    add some

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+//webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+    	rules: [
+    		{
+    			test: /\.js$/,
+    			loader: "babel-loader",
+    			options: {
+    				presets: ["env"]
+    			}
+    		}
+    	]
+    }
+};
